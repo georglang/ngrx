5ff794632d60be3b9a6cd45eeeea853e8fe1942b
Georg Lang <mail@georglang.com>
2020-12-17 15:02:23 +0100

    @ngrx/store-devtools added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment.prod';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
 import { ProductModule } from './product/product.module';
 
 @NgModule({
@@ -13,6 +15,11 @@ import { ProductModule } from './product/product.module';
     AppRoutingModule,
     ProductModule,
     StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      name: 'Georg Test Ngrx',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
